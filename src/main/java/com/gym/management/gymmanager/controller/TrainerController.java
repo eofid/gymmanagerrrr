d6efc3fcd5af930828cd03eefca636b49c5d2466
eofid <arhipadynec@gmail.com>
2025-04-18 10:57:58 +0300

    Update TrainerController.java

--- src/main/java/com/gym/management/gymmanager/controller/TrainerController.java
+++ src/main/java/com/gym/management/gymmanager/controller/TrainerController.java
@@ -2,7 +2,6 @@ package com.gym.management.gymmanager.controller;
 
 import com.gym.management.gymmanager.model.Trainer;
 import com.gym.management.gymmanager.service.TrainerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
